5c0a0b73d236a5c00681badfca1fdeb0825a25ee
Ramesh KC <kcramesh8@gmail.com>
2018-01-02 20:13:32 -0600

    pass model value to jsp file.

--- src/main/java/com/tutorial/springboot/controller/LoginController.java
+++ src/main/java/com/tutorial/springboot/controller/LoginController.java
@@ -1,14 +1,19 @@
 package com.tutorial.springboot.controller;
 
 import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class LoginController {
 
-	@RequestMapping("/login")
-	public String loginMessage() {
+	@RequestMapping(value = "/login",  method = RequestMethod.GET)
+	public String loginMessage(@RequestParam String name, ModelMap model) {
+		model.put("name", name);
 		return "login";
 	}
 }
--- src/main/webapp/WEB-INF/jsp/login.jsp
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -3,6 +3,7 @@
     <title>Web Application </title>
 </head>
 <body>
+    <h1>Hello: ${name}</h1>
     <p>My first Application</p>
 </body>
 </html>
